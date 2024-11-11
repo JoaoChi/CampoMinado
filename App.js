bd10a52776f57c2b534180d9569c6defe022f8ae
Joao Gabriel Chiarello <joao.chiarello@unochapeco.edu.br>
2024-11-11 15:47:34 -0300

    criado funções para lógica do curso e o minefield

--- App.js
+++ App.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import params from './src/params';
 import Field from './src/components/Field';
+import mineField from './src/components/mineField';
+import { createdMinedBoard } from './src/functions';
 
 export default class App extends Component {
-  render(){
+
+  constructor(props){
+    super(props)
+    this.state =  this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: createdMinedBoard(rows, cols, this.minesAmount),
+    }
+  }
+
+  render() {
     return (
-    
+
       <View style={styles.sectionContainer}>
         <Text style={styles.welcome}>Iniciando o mines!</Text>
         <Text style={styles.introductions}>Tamanho da grade:
           {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
-        <Field/>
-        <Field opened/>
+        <Field />
+        <Field opened />
         <Field opened nearMines={1} />
         <Field opened nearMines={2} />
         <Field opened nearMines={4} />
@@ -30,7 +44,7 @@ export default class App extends Component {
         <Field mined />
         <Field mined opened />
         <Field mined opened exploded />
-        <Field flagged opened/>
+        <Field flagged opened />
         <Field flagged />
       </View>
     );
